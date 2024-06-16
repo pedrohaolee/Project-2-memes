e7519bf752b217a49997e18b1013f4a00c30c053
Hao Li <pedrohaolee@gmail.com>
2024-06-16 17:40:27 +0800

    Add APIKEY

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -3,16 +3,16 @@ import styles from "./Book.module.css";
 import UpdateModal from "./UpdateModal";
 const Book = (props) => {
   // const [showUpdateModal, setShowUpdateModal] = useState(false);
-  // const deleteBook = async () => {
-  //   const res = await fetch(
-  //     import.meta.env.VITE_SERVER + "/lesson/books/" + props.id,
-  //     { method: "DELETE", headers: { "Content-Type": "application/json" } }
-  //   );
-  //   if (!res.ok) {
-  //     throw new Error("error deleting book");
-  //   }
-  //   props.getData();
-  // };
+  const favMeme = async () => {
+    const res = await fetch(
+      import.meta.env.VITE_SERVER + "/lesson/books/" + props.id,
+      { method: "DELETE", headers: { "Content-Type": "application/json" } }
+    );
+    if (!res.ok) {
+      throw new Error("error deleting book");
+    }
+    props.getData();
+  };
 
   return (
     <>
@@ -28,15 +28,15 @@ const Book = (props) => {
       <div className={`row ${styles.book}`}>
         <div className="col-sm-3">{props.name}</div>
         <img className="col-sm-3" src={props.url} />
-        <div className="col-sm-2">{props.height}</div>
+        {/* <div className="col-sm-2">{props.height}</div>
         <div className="col-sm-2">{props.width}</div>
-        <div className="col-sm-2"></div>
+        <div className="col-sm-2"></div> */}
         {/* <button className="col-sm-2" onClick={() => setShowUpdateModal(true)}>
           update
-        </button>
-        <button className="col-sm-2" onClick={deleteBook}>
-          delete
         </button> */}
+        <button className="col-sm-2" onClick={favMeme}>
+          favorite
+        </button>
       </div>
     </>
   );
